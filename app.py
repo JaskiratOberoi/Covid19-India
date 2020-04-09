cd5598157b459fa2cb0edead4d2fcbdea5a9259f
Jaskirat Singh Oberoi <Jso101196@gmail.com>
2020-04-09 16:41:27 +0530

    Date formatiing changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import json
 import pprint
+import calendar
 
 app = Flask(__name__)
 
@@ -14,8 +15,10 @@ def index():
     TotalDeaths = data["statewise"][0]["deaths"]
     TotalRecovered = data["statewise"][0]["recovered"]
     LastUpdate = data["statewise"][0]["lastupdatedtime"]
-    pprint.pprint(TotalConfirmed)
-    return render_template('index.html', totalConfirmed = TotalConfirmed, totalActive = TotalActive, totalDeaths = TotalDeaths, totalRecovered = TotalRecovered, lastUpdate = LastUpdate)
+    month = int(LastUpdate[4:5])
+    UpdateDate = LastUpdate[:2] + " " + calendar.month_name[month] + " " + LastUpdate[6:10] + " " + LastUpdate[11:13] + "00Hrs"
+    # pprint.pprint(TotalConfirmed)
+    return render_template('index.html', totalConfirmed = TotalConfirmed, totalActive = TotalActive, totalDeaths = TotalDeaths, totalRecovered = TotalRecovered, lastUpdate = UpdateDate)
   
 
 
