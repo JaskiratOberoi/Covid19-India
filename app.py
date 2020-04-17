f489c9d7114202cd1c2b967a668ed624f713fa26
Jaskirat Singh Oberoi <Jso101196@gmail.com>
2020-04-17 11:45:35 +0530

    Added all states on mobile

--- app.py
+++ app.py
@@ -9,15 +9,10 @@ app = Flask(__name__)
 def index():
     data = requests.get('https://api.covid19india.org/data.json')
     data = data.json()
-    TotalConfirmed = data["statewise"][0]["confirmed"]
-    TotalActive = data["statewise"][0]["active"]
-    TotalDeaths = data["statewise"][0]["deaths"]
-    TotalRecovered = data["statewise"][0]["recovered"]
     LastUpdate = data["statewise"][0]["lastupdatedtime"]
     month = int(LastUpdate[4:5])
     UpdateDate = LastUpdate[:2] + " " + calendar.month_name[month] + " " + LastUpdate[6:10] + " " + LastUpdate[11:13] + ":00Hrs"
-    # caseData = json.dumps(data["statewise"])
-    return render_template('index.html', caseData = data['statewise'], lastUpdate = LastUpdate)
+    return render_template('index.html', caseData = data['statewise'], lastUpdate = UpdateDate)
   
 
 if __name__ == '__main__':
